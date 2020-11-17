6984668de79fd38504bd09a0d60ed7cfef4b2128
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-11-17 20:31:37 -0300

    uploud UI files

---
+++ src/UI/Header.svelte
@@ -0,0 +1,25 @@
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: #026AA7;
+		background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #026AA7 100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.26);
+  }
+
+  h1 {
+    color: white;
+    font-family: "Roboto Slab", serif;
+    margin: 0;
+  }
+</style>
+
+<header>
+  <h1>Product Register</h1>
+</header>
